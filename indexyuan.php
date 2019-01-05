458e95cad2bf276248137483a2599ca8e46715bc
mikey <leeshukay@gmail.com>
2019-01-05 21:45:52 +0800

    haha

---
+++ indexyuan.php
@@ -0,0 +1,27 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>小宁宁&大凯凯</title>
+<style type="text/css">
+a:link {
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+}
+a:hover {
+	text-decoration: none;
+}
+a:active {
+	text-decoration: none;
+}
+</style>
+</head>
+
+<body bgcolor="#84EBF0">
+<p><a href="hi.php"><img src="8.jpg" width="422" height="237" alt=""/></a></p>
+<p>&nbsp;</p>
+<p><em><strong><a href="liucheng.php">家教应聘流程</a></strong></em></p>
+</body>
+</html>
---
+++ liucheng.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>家教应聘流程</title>
+<style type="text/css">
+body,td,th {
+	font-size: 36px;
+}
+</style>
+</head>
+
+<body background="26.png" text="#645B4A">
+<p align="center"><strong>家教应聘流程 </strong></p>
+<p align="center"><strong>1、核对确认</strong></p>
+<p align="center">看到心仪家教，请先查清楚路线并确认距离和时间是否可接受。 </p>
+<p align="center">2、<strong>登陆网址预约、联系管理员 </strong></p>
+<p align="center">查清楚路线并确认距离和时间可接受后，去该家教的预约网址预约。 <br>
+  预约好后私聊发布家教的管理员。 <br>
+不预约不予考虑。 </p>
+<p align="center">3、<strong>通知试课</strong></p>
+<p align="center">收到教员信息后，管理员会转发给家长选择； <br>
+  家长选定试教教员后，管理员会联系该试课教员 </p>
+<p align="center">4、<strong>付费联系 </strong></p>
+<p align="center">教员按照流程支付信息费之后，管理员提供家长联系方式，教员应及时联系家长确认。 </p>
+</body>
+</html>
