79cacacca4fbac3f5786655b8e5be8fe2648ccb2
ShuKayLee <leeshukay@gmail.com>
2019-01-05 21:08:45 +0800

    delete

---
+++ index.php
@@ -0,0 +1,18 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>有标题文档</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+<p>Hello World </p>
+<p>安</p>
+<p><img src="../June_odd-eyed-cat_cropped.jpg" width="175" height="147" alt=""/></p>
+<p>--by Kay</p>
+</body>
+</html>
